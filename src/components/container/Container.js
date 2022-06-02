d39709711c36a74f28dfe18ddcc97d59d72e00b4
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-02 17:08:18 -0500

    Add basic container component

---
+++ src/components/container/Container.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import './Container.css';
+
+const Container = () => (
+  <>
+    <Nav />
+    <Routes>
+    </Routes>
+  </>
+);
+
+export default Container;
