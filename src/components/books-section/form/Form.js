a0cd4493c70c2940bd7e28f3b8ec851ca649159e
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-02 23:13:40 -0500

    Fix books section styles

--- src/components/books-section/form/Form.js
+++ src/components/books-section/form/Form.js
@@ -1,20 +1,24 @@
 import React from 'react';
 
+import './Form.css';
+
 const Form = () => (
   <div>
     <form className="formBookAdd">
       <h3>ADD NEW BOOK</h3>
-      <label className="inputTitle" htmlFor="text">
-        <input id="title" name="title" type="text" placeholder="Title" />
-      </label>
-      <select className="categorySelect" defaultValue="">
-        <option value="" disabled hidden>Category</option>
-        <option value="adventure">adventure</option>
-        <option value="maths">maths</option>
-        <option value="violence">violence</option>
-        <option value="time travel">time travel</option>
-      </select>
-      <button className="addBookBtn" type="submit">ADD BOOK</button>
+      <div>
+        <label className="inputTitle" htmlFor="text">
+          <input id="title" name="title" type="text" placeholder="Title" />
+        </label>
+        <select className="categorySelect" defaultValue="">
+          <option value="" disabled hidden>Category</option>
+          <option value="adventure">adventure</option>
+          <option value="maths">maths</option>
+          <option value="violence">violence</option>
+          <option value="time travel">time travel</option>
+        </select>
+        <button className="addBookBtn" type="submit">ADD BOOK</button>
+      </div>
     </form>
   </div>
 );
