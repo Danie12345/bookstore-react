310dc7cd6c17c8da8d63d18ce65a5859dcc35209
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-06 16:40:51 -0500

    Add categories reducer and action

---
+++ src/redux/categories/categories.js
@@ -0,0 +1,18 @@
+// Actions
+const STATUS_CHECKED = 'STATUS_CHECKED';
+const defaultState = [];
+
+// Reducer
+export default reducer = (state = defaultState, action = {}) => {
+  switch (action.type) {
+    case STATUS_CHECKED:
+      return 'Under construction';
+    default:
+      return state;
+  }
+};
+
+// Action Creators
+export function checkStatus() {
+  return { type: STATUS_CHECKED };
+}
