9cdf39703fda39c47e132ce954dbcc6f9fe09189
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-06 16:29:41 -0500

    Add books reducer and actions

---
+++ src/redux/books/books.js
@@ -0,0 +1,30 @@
+// Actions
+const ADD_BOOK = 'ADD_BOOK';
+const REM_BOOK = 'REM_BOOK';
+const defaultState = [];
+
+// Reducer
+export default reducer = (state = defaultState, action = {}) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [
+        ...state,
+        action.payload
+      ];
+    case REM_BOOK:
+      return [
+        state.filter((item) => item !== action.payload)
+      ];
+    default:
+      return state;
+  }
+};
+
+// Action Creators
+export function addBook(obj) {
+  return { type: ADD_BOOK, payload: obj };
+}
+
+export function removeBook(obj) {
+  return { type: ADD_BOOK, payload: obj };
+}
