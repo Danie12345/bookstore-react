89fb7a5f580f422a8e8d6b02a8a4cd41afcc73f7
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-07 00:41:22 -0500

    Move provider to top level in index.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Container from './components/container/Container';
-import store from './redux/configureStore';
 
 const App = () => (
-  <Provider store={store}>
+  <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL}>
       <Container />
     </Router>
-  </Provider>
+  </React.StrictMode>
 );
 
 export default App;
