52f128a764a3e30fa008b9bbecdf074329fa436c
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-07 21:19:53 -0500

    Imported applyMiddleware and thunk

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,6 @@
-import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { combineReducers, applyMiddleware, legacy_createStore as createStore } from '@reduxjs/toolkit';
+
+import thunk from 'redux-thunk';
 
 import booksReducer from './books/books';
 import categoriesReducer from './categories/categories';
@@ -8,8 +10,9 @@ const reducers = combineReducers({
   categories: categoriesReducer,
 });
 
-const store = configureStore({
-  reducer: reducers,
-});
+const store = createStore(
+  reducers,
+  applyMiddleware(thunk),
+);
 
 export default store;
