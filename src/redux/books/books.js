334bb300bff68a660a6a369b58024c0277b1204c
Daniel Malo <danielmaloosorio@gmail.com>
2022-06-08 14:59:27 -0500

    Action makers now use API calls

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,26 +1,9 @@
+import BookstoreDataService from '../../services/BookstoreService';
+
 // Actions
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-const defaultState = [
-  {
-    id: '1',
-    author: 'Daniel',
-    title: 'How to get away with mass murder',
-    category: 'adventure',
-  },
-  {
-    id: '2',
-    author: 'Arturo',
-    title: 'How To Make A Professional Resume',
-    category: 'adventure',
-  },
-  {
-    id: '3',
-    author: 'Luis',
-    title: 'Polygons Are Old-School, Multigons Are In',
-    category: 'maths',
-  },
-];
+const defaultState = [];
 
 // Reducer
 export default function reducer(state = defaultState, action = {}) {
@@ -38,10 +21,22 @@ export default function reducer(state = defaultState, action = {}) {
 }
 
 // Action Creators
-export function addBook(obj) {
-  return { type: ADD_BOOK, payload: obj };
-}
+export const addBook = (obj) => async (dispatch) => {
+  try {
+    const res = await BookstoreDataService.createBook(obj);
+    dispatch({ type: ADD_BOOK, payload: obj });
+    return Promise.resolve(res.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
 
-export function removeBook(obj) {
-  return { type: REMOVE_BOOK, payload: obj };
-}
+export const removeBook = (obj) => async (dispatch) => {
+  try {
+    const res = await BookstoreDataService.deleteBook(obj.id);
+    dispatch({ type: REMOVE_BOOK, payload: obj });
+    return Promise.resolve(res.data);
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
